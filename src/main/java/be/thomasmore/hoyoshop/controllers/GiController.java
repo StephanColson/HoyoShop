42288ee6abe1b7c073a8ff37db7da527f08530a4
StephanColson <151721516+StephanColson@users.noreply.github.com>
2024-11-26 11:45:19 +0100

    adding navigation between the pages

--- src/main/java/be/thomasmore/hoyoshop/controllers/GiController.java
+++ src/main/java/be/thomasmore/hoyoshop/controllers/GiController.java
@@ -1,4 +1,12 @@
 package be.thomasmore.hoyoshop.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class GiController {
+    @GetMapping("/gi")
+    public String gi() {
+        return "gi";
+    }
 }
--- src/main/java/be/thomasmore/hoyoshop/controllers/ZzzController.java
+++ src/main/java/be/thomasmore/hoyoshop/controllers/ZzzController.java
@@ -1,4 +1,12 @@
 package be.thomasmore.hoyoshop.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ZzzController {
+    @GetMapping("/zzz")
+    public String zzz() {
+        return "zzz";
+    }
 }
