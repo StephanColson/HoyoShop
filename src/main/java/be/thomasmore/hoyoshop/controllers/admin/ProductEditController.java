8d3b14b9930607d61cc101446d0e5951c6edd529
StephanColson <151721516+StephanColson@users.noreply.github.com>
2025-03-10 16:54:39 +0100

    Multiple edit page added. hi3rdedit page is the only functional one currently. Updat the name of the product, description and the price. Note: Date and time still need to be added on both location (Detail page and the Edit page for the update). Edit page still needs an input on image change.

--- src/main/java/be/thomasmore/hoyoshop/controllers/admin/ProductEditController.java
+++ src/main/java/be/thomasmore/hoyoshop/controllers/admin/ProductEditController.java
@@ -1,9 +1,54 @@
 package be.thomasmore.hoyoshop.controllers.admin;
 
+import be.thomasmore.hoyoshop.models.Product;
+import be.thomasmore.hoyoshop.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
 public class ProductEditController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @ModelAttribute("product")
+    public Product findProduct(@PathVariable (required = false) int id) {
+
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            return optionalProduct.get();
+        }
+        return null;
+    }
+
+    @GetMapping("/hi3rdedit/{id}")
+    public String hi3rd(@PathVariable int id) {
+        return "/admin/hi3rdedit";
+    }
+
+    @PostMapping("/hi3rdedit/{id}")
+    public String hi3rdeidt(@PathVariable int id, Product product) {
+        productRepository.save(product);
+        return "redirect:/hi3rdDetails/" + id;
+    }
+
+    @GetMapping("/giedit/{id}")
+    public String gi(@PathVariable int id) {
+        return "redirect:/admin/giedit";
+    }
+
+    @GetMapping("/hsredit/{id}")
+    public String hsr(@PathVariable int id) {
+        return "redirect:/admin/hsredit";
+    }
+
+    @GetMapping("/zzzedit/{id}")
+    public String zzz(@PathVariable int id) {
+        return "/admin/zzzedit";
+    }
 }
