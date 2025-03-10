974527709d2fb487aee49029a0f7b588d9af94d4
StephanColson <151721516+StephanColson@users.noreply.github.com>
2025-03-10 17:22:59 +0100

    All edit pages are partially done. Need to add a few more input fields: Category, Character, Game, Date/Time and Outfit.
    Other Edit pages are for later versions (Like Edit Character)

--- src/main/java/be/thomasmore/hoyoshop/controllers/admin/ProductEditController.java
+++ src/main/java/be/thomasmore/hoyoshop/controllers/admin/ProductEditController.java
@@ -39,16 +39,34 @@ public class ProductEditController {
 
     @GetMapping("/giedit/{id}")
     public String gi(@PathVariable int id) {
-        return "redirect:/admin/giedit";
+        return "/admin/giedit";
+    }
+
+    @PostMapping("/giedit/{id}")
+    public String giedit(@PathVariable int id, Product product) {
+        productRepository.save(product);
+        return "redirect:/giDetails/" + id;
     }
 
     @GetMapping("/hsredit/{id}")
     public String hsr(@PathVariable int id) {
-        return "redirect:/admin/hsredit";
+        return "/admin/hsredit";
+    }
+
+    @PostMapping("/hsredit/{id}")
+    public String hsredit(@PathVariable int id, Product product) {
+        productRepository.save(product);
+        return "redirect:/hsrDetails/" + id;
     }
 
     @GetMapping("/zzzedit/{id}")
     public String zzz(@PathVariable int id) {
         return "/admin/zzzedit";
+    }
+
+    @PostMapping("/zzzedit/{id}")
+    public String zzzedit(@PathVariable int id, Product product) {
+        productRepository.save(product);
+        return "redirect:/zzzDetails/" + id;
     }
 }
