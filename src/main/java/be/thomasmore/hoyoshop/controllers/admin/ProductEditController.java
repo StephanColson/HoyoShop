875bf8ad6892e68e72191b55edc1f9777ca14299
StephanColson <151721516+StephanColson@users.noreply.github.com>
2025-03-10 17:23:41 +0100

    Patch: Removed unused import

--- src/main/java/be/thomasmore/hoyoshop/controllers/admin/ProductEditController.java
+++ src/main/java/be/thomasmore/hoyoshop/controllers/admin/ProductEditController.java
@@ -4,7 +4,6 @@ import be.thomasmore.hoyoshop.models.Product;
 import be.thomasmore.hoyoshop.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
