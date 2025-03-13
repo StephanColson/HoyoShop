5bf39b9dac8b393c4b7da61fa98696ccfe76f42d
StephanColson <151721516+StephanColson@users.noreply.github.com>
2025-03-13 09:59:42 +0100

    sec:authorize, Hiding the buttons depending on if you are authenticated. Add a new dependency

--- src/main/java/be/thomasmore/hoyoshop/controllers/HomeController.java
+++ src/main/java/be/thomasmore/hoyoshop/controllers/HomeController.java
@@ -4,16 +4,20 @@ import be.thomasmore.hoyoshop.models.Game;
 import be.thomasmore.hoyoshop.models.GameCharacter;
 import be.thomasmore.hoyoshop.repositories.GameCharacterRepository;
 import be.thomasmore.hoyoshop.repositories.GameRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
 import java.util.Optional;
 
 @Controller
 public class HomeController {
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
     @Autowired
     private GameRepository gameRepository;
 
@@ -21,7 +25,9 @@ public class HomeController {
     private GameCharacterRepository gameCharacterRepository;
 
     @GetMapping({"/", "/home"})
-    public String home(Model model) {
+    public String home(Model model, Principal principal) {
+        final String username = principal != null ? principal.getName() : null;
+        logger.info("Homepage - Logged is as: " + username);
         final Iterable<Game> games = gameRepository.findAll();
         model.addAttribute("games", games);
         model.addAttribute("activePage", "home");
